a9b0514dba9840b3f028d07c4fa1e92f147dd133
Tommy <tommychang@knights.ucf.edu>
2021-06-06 22:46:13 -0400

    init2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,9 +2,9 @@ package org.example;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Tommy Chang
  */
 public class App 
 {
