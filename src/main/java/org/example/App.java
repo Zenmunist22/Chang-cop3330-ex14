7e2c3989b61336d93c8fa8f8a66c0dfc579ef75b
Tommy <tommychang@knights.ucf.edu>
2021-06-06 22:39:20 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String state;
+        double orderAmount = 0, tax = 0,taxAmount = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is the order amount? ");
+        orderAmount = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("What is the state? ");
+        state = sc.nextLine();
+        if (state.equals("WI")) {
+            tax = .055;
+            System.out.printf("The subtotal is: $%.2f\n", orderAmount);
+            taxAmount = (int) (.5 + (100 * (orderAmount * tax)));
+            taxAmount /= 100;
+            System.out.printf("The tax is $%1.2f\n", taxAmount);
+        }
+        System.out.printf("The total is $%.2f", orderAmount + taxAmount );
+    }
+}
